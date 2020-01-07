c997690af3811cb4c17438f900e7f070c5bffa2d
h424568209 <424568209@qq.com>
2020-01-07 23:02:13 +0800

    唐诗分析 爬虫初级

--- src/main/java/lab/HttpDemo.java
+++ src/main/java/lab/HttpDemo.java
@@ -2,6 +2,7 @@ package lab;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomText;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -17,7 +18,9 @@ public class HttpDemo {
         webClient.getOptions().setJavaScriptEnabled(false);
         //关闭了浏览器的CSS执行引擎，不再执行网页中的css布局
         webClient.getOptions().setCssEnabled(false);
-        HtmlPage htmlPage = webClient.getPage("https://so.gushiwen.org/gushi/tangshi.aspx");
+        HtmlPage htmlPage = webClient.getPage("https://so.gushiwen.org/shiwenv_45c396367f59.aspx");
+
+        /*
         System.out.println(htmlPage);
      File file =new File("唐诗三百首\\列表页.html");
      file.delete();
@@ -30,12 +33,53 @@ public class HttpDemo {
        for(HtmlElement e :elements){
            System.out.println(e);
        }
-       */
+
+
         HtmlElement divElement = elements.get(0);
        List<HtmlElement> aElement = divElement.getElementsByAttribute("a","target","_blank");
        for(HtmlElement e : aElement){
            System.out.println(e);
        }
         System.out.println(aElement.size());
+       */
+
+
+        HtmlElement body = htmlPage.getBody();
+         /*
+            List<HtmlElement> elements = body.getElementsByAttribute(
+                    "div",
+                    "class",
+                    "contson"
+            );
+
+            for (HtmlElement element : elements) {
+                System.out.println(element);
+            }
+
+            System.out.println(elements.get(0).getTextContent().trim());
+             */
+
+        // 标题
+        {
+            String xpath = "//div[@class='cont']/h1/text()";
+            Object o = body.getByXPath(xpath).get(0);DomText domText = (DomText)o;
+            System.out.println(domText.asText());
+        }
+        {
+            String xpath = "//div[@class='cont']/p[@class='source']/a[1]/text()";
+            Object o = body.getByXPath(xpath).get(0);DomText domText = (DomText)o;
+            System.out.println(domText.asText());
+        }
+        {
+            String xpath = "//div[@class='cont']/p[@class='source']/a[2]/text()";
+            Object o = body.getByXPath(xpath).get(0);DomText domText = (DomText)o;
+            System.out.println(domText.asText());
+        }
+        {
+            String xpath = "//div[@class='cont']/div[@class='contson']";
+            Object o = body.getByXPath(xpath).get(0);
+            HtmlElement element = (HtmlElement)o;
+            System.out.println(element.getTextContent().trim());
+        }
     }
 }
---
+++ src/main/java/lab/SHA256.java
@@ -0,0 +1,19 @@
+package lab;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256 {
+    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        String s = "你好啊";
+        byte[]bytes = s.getBytes("UTF-8");
+        messageDigest.update(bytes);
+        byte[]result = messageDigest.digest();
+        System.out.println(result.length);
+        for(byte b : result){
+            System.out.printf("%02x",b);
+        }
+    }
+}
